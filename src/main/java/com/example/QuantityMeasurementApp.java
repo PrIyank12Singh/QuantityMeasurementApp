d62df74dddcd0d3fae3f4ca90253e003de7cb076
PrIyank12Singh <115387653+PrIyank12Singh@users.noreply.github.com>
2026-02-18 22:16:38 +0530

    [Priyank] added UC-1

--- src/main/java/com/example/QuantityMeasurementApp.java
+++ src/main/java/com/example/QuantityMeasurementApp.java
@@ -1,9 +1,35 @@
 package com.example;
 
-public class QuantityMeasurementApp {
+public class QuantityMeasurementApp 
+{
+    public static class Feet {
+        private final double value;
+
+        public Feet(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            
+            if (obj == null || getClass() != obj.getClass()) {
+                return false;
+            }
+            
+            Feet other = (Feet) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+
+        @Override
+        public int hashCode() {
+            return Double.hashCode(value);
+        }
+    }
 
     public static void main(String[] args) {
-
         Feet feet1 = new Feet(1.0);
         Feet feet2 = new Feet(1.0);
         Feet feet3 = new Feet(2.0);
